8e9349448e07882ee0b5f73a527bc5ae9744b708
rodrigogons <rodrigogoncalveess@gmail.com>
2017-06-07 21:48:52 -0300

    fill changes and new ULA

--- projeto_integrador_2017_s1/projeto_integrador_2017_s1/Scripts/Ula.js
+++ projeto_integrador_2017_s1/projeto_integrador_2017_s1/Scripts/Ula.js
@@ -1,91 +1,124 @@
-var btt = document.querySelector(".go");
-var bin = document.querySelector(".v1");
-var dec = document.querySelector(".v2");
-var sRes = document.querySelector(".sRes");
-var carrier = document.querySelector(".MSB");
-var overflow = document.querySelector(".OVR");
-var acao = document.querySelector(".S");
-var cn = document.querySelector(".cn");
-var pl1 = document.querySelector(".pl1");
-var pl2 = document.querySelector(".pl2");
+function start(act) {
+    var valorA = document.querySelector(".valorA").value;
+    var valorB = document.querySelector(".valorB").value;
 
-
-function calculo(act) {
-    var sVal1 = document.querySelector(".sVal1").value;
-    var sVal2 = document.querySelector(".sVal2").value
-
-	// preset alterar o resultado e para a função
-	if (act == "set") {
-		sRes.innerHTML = "1111";
-		return;
-	}
-
-    if (checkVals(sVal1) && checkVals(sVal2)) {
-        sVal1 = reSize(sVal1);
-        sVal2 = reSize(sVal2);
-        bin.innerHTML = sVal1;
-        dec.innerHTML = sVal2;
-        v1 = parseInt(sVal1, 2);
-        v2 = parseInt(sVal2, 2);
+    if (checkVals(valorA) && checkVals(valorB)) {
+        valorA = reSize(valorA);
+        valorB = reSize(valorB);
+		fillValores("a", valorA, 4);
+		fillValores("b", valorB, 4);
 
         // contas de acordo com o que é requerido
-
-        if (act == "aMenosB") {
-            result = v1 - v2;
-            acao.innerHTML = "010";
-            cn.innerHTML = "1";
-        }
-        if (act == "bMenosA") {
-            result = v2 - v1;
-            acao.innerHTML = "001";
-            cn.innerHTML = "1";
-        }
-        if (act == "aMaisB") {
-            result = v1 + v2;
-            acao.innerHTML = "011";
-            cn.innerHTML = "0";
-        }
-
-        overflow.innerHTML = 0;
-        carrier.innerHTML = 0;
-        overflow.classList.remove("over");
-        carrier.classList.remove("over");
-        pl1.classList.remove("over");
-        pl2.classList.remove("over");
-
-        // em caso de o valor sair negativo
-        if (result < 0) {
-            carrier.innerHTML = 1;
-            carrier.classList.add("over");
-            pl1.classList.add("over");
-            result = result * -1;
-        }
-		 // em caso do valor ser > 15 ou > 7 caso negativo
-        if (result > 15 || (cn.innerHTML == "1" && result > 8)) {
-            result = 0;
-            overflow.innerHTML = 1;
-            overflow.classList.add("over");
-            pl2.classList.add("over");
-        }
-
-        resBin = reSize((result >>> 0).toString(2));
-        sRes.innerHTML = resBin.split('').reverse().join('');
-    } else alert("Um dos valores não é valido");
+        if (act == "asubb") asubb(valorA, valorB, "010");
+        if (act == "bsuba") bsuba(valorA, valorB, "001");
+        if (act == "soma") soma(valorA, valorB, "011");
+        if (act == "aeb") aeb(valorA, valorB, "110");
+        if (act == "aoub") aoub(valorA, valorB, "101");
+        if (act == "axorb") axorb(valorA, valorB, "100");
+    }
+	else alert("Um dos valores não é valido");
 }
 
-// verifica se a string digitada contem apenas 1's e 0's
+// Realiza valorB - valorA
+function asubb(valorA, valorB, s) {
+	var result;
+    fillVal("cn", "1");
+	fillValores("s", s, 3);
+	subtra(valorA, valorB);
+}
+
+// Realiza valorA - valorB
+function bsuba(valorA, valorB, s) {
+    fillVal("cn", "1");
+	fillValores("s", s, 3);
+	subtra(valorB, valorA);
+}
+
+// Soma os dois valores
+function soma(valorA, valorB, s) {
+	var result;
+    fillVal("cn", "0");
+    fillValores("s", s, 3);
+	result = parseInt(valorA, 2) + parseInt(valorB, 2);
+	if (result > 15) {
+		fillValores("f", "0", 4);
+		fillVal("ovr", "1");
+		return;
+	}
+	fillValores("f", (result >>> 0).toString(2), 4);
+}
+
+// Comando and: se valorA e valorB = 1 -> f = 1
+function aeb(valorA, valorB, s) {
+    fillVal("cn", "0");
+    fillValores("s", s, 4);
+    for (var i = 0; i < 4; i++) {
+        if (valorA[i] == "1" && valorB[i] == "1") document.querySelector(".f"+i).innerHTML = "1";
+        else document.querySelector(".f"+i).innerHTML = "0";
+    }
+}
+
+// Comando or: se valorA ou valorB = 1 -> F = 1
+function aoub(valorA, valorB, s) {
+    fillVal("cn", "0");
+    fillValores("s", s, 4);
+    for (var i = 0; i < 4; i++) {
+        if (valorA[i] == "1" || valorB[i] == "1") document.querySelector(".f"+i).innerHTML = "1";
+        else document.querySelector(".f"+i).innerHTML = "0";
+    }
+}
+
+// Comando xor: valorA diferente valorB = 1
+function axorb(valorA, valorB, s) {
+    fillVal("cn", "0");
+    fillValores("s", s, 4);
+    for (var i = 0; i < 4; i++) {
+        if (valorA[i] == valorB[i]) document.querySelector(".f"+i).innerHTML = "0";
+        else document.querySelector(".f"+i).innerHTML = "1";
+    }
+}
+
+// Subtração generalizado
+function subtra(valA, valB){
+	var result = parseInt(valA, 2) - parseInt(valB, 2);
+	if (result < 0) {
+		fillVal("cn4", "1");
+		result *= -1;
+	}
+	if (result > 15) {
+		fillValores("f", "0", 4);
+		fillVal("ovr", "1");
+		return;
+	}
+	result = (result >>> 0).toString(2)
+	result = reSize(result);
+	fillValores("f", result, 4);
+}
+
+// Zera todos os valores
+function limpar() {
+	fillValores("f", "0", 4);
+	fillValores("a", "0", 4);
+	fillValores("b", "0", 4);
+	fillValores("s", "0", 3);
+	fillVal("cn", "0");
+	fillVal("cn4", "0");
+	fillVal("ovr", "0");
+}
+
+// Chama limpar() e em seguida altera s = 111 e f = 1111
+function setar() {
+    limpar();
+	fillValores("f", "1111", 4);
+	fillValores("s", "111", 3);
+}
+
+// Verifica se a string digitada contem apenas 1's e 0's
 function checkVals(val) {
-    for (i = 0; i < val.length; i++) {
-        if (val[i] != "0" && val[i] != "1") {
-            return false;
-        }
+    for (var i = 0; i < val.length; i++) {
+        if (val[i] != "0" && val[i] != "1") return false;
     }
     return true;
-}
-
-// Converte valores para inteiros
-function convert(num) {
-    return parseInt(num, 2);
 }
 
 // Corrige o tamanho dos valores para 4 digitos
@@ -94,4 +127,17 @@ function reSize(val) {
         return Array(5 - val.length).join("0") + val;
     }
     return val;
+}
+
+// Simplifica a saida de um valor
+function fillVal(saida, inform) {
+	document.querySelector("."+saida).innerHTML = inform;
+}
+
+// Simplifica a saida de n valores
+function fillValores(saida, inform, vz) {
+	if (inform.length < 2) inform = inform.repeat(vz);
+	for (var i = 0; i < vz; i++) {
+		document.querySelector("."+saida+i).innerHTML = inform[i];
+	}
 }
